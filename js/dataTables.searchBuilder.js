4ccb793ad550cef1838356efe3350b1e44a1733b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2020-11-06 12:35:27 +0000

    fix: Fix issue with null values in SearchBuilder throwing an error
    Jira Issue DD-1714
    
    Sync to source repo @e2af334c041fe0d84660249f0e64a818a1fe77e6

--- js/dataTables.searchBuilder.js
+++ js/dataTables.searchBuilder.js
@@ -1493,7 +1493,7 @@
 	            value: [
 	                $('<select disabled/>').addClass(this.classes.value).addClass(this.classes.dropDown).addClass(this.classes.italic)
 	            ],
-	            valueTitle: $('<option value="" selected/>').text(this.s.dt.i18n('searchBuilder.value', i18n.value))
+	            valueTitle: $('<option value="--valueTitle--" selected/>').text(this.s.dt.i18n('searchBuilder.value', i18n.value))
 	        };
 	        // If the greyscale option is selected then add the class to add the grey colour to SearchBuilder
 	        if (this.c.greyscale) {
@@ -2100,8 +2100,12 @@
 	            };
 	            // Add text and value, stripping out any html if that is the column type
 	            var opt = $('<option>', {
-	                text: typeof value.text === 'string' ? value.text.replace(/(<([^>]+)>)/ig, '') : value.text,
-	                value: that.s.type.indexOf('html') !== -1 ? value.filter.replace(/(<([^>]+)>)/ig, '') : value.filter
+	                text: typeof value.text === 'string' ?
+	                    value.text.replace(/(<([^>]+)>)/ig, '') :
+	                    value.text,
+	                value: that.s.type.indexOf('html') !== -1 && value.filter !== null ?
+	                    value.filter.replace(/(<([^>]+)>)/ig, '') :
+	                    value.filter
 	            })
 	                .addClass(that.classes.option)
 	                .addClass(that.classes.notItalic);
