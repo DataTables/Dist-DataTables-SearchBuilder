f0c6494b178d63f78a74758ef0b09ec62ef84dca
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-02-24 15:10:52 +0000

    84f7a616b44006b222cce45bfe0ff9d6b5784a90 fix: Fix issue with moment and luxon reparsing dates that have already been parsed.
    Jira Issue DD-2475
    
    Sync to source repo @84f7a616b44006b222cce45bfe0ff9d6b5784a90

--- js/dataTables.searchBuilder.js
+++ js/dataTables.searchBuilder.js
@@ -288,14 +288,18 @@
                 }
                 else if (this.s.type.includes('moment')) {
                     for (var i = 0; i < this.s.value.length; i++) {
-                        if (this.s.value[i] && this.s.value[i].length > 0) {
+                        if (this.s.value[i] &&
+                            this.s.value[i].length > 0 &&
+                            moment(this.s.value[i], this.s.dateFormat, true).isValid()) {
                             this.s.value[i] = moment(this.s.value[i], this.s.dateFormat).toISOString();
                         }
                     }
                 }
                 else if (this.s.type.includes('luxon')) {
                     for (var i = 0; i < this.s.value.length; i++) {
-                        if (this.s.value[i] && this.s.value[i].length > 0) {
+                        if (this.s.value[i] &&
+                            this.s.value[i].length > 0 &&
+                            luxon.DateTime.fromFormat(this.s.value[i], this.s.dateFormat).invalid === null) {
                             this.s.value[i] = luxon.DateTime.fromFormat(this.s.value[i], this.s.dateFormat).toISO();
                         }
                     }
