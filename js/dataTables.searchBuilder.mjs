70eef676b9f2fda0c48c1cde1e384f06e5054f69
dtbuild <dtbuild@rigil>
2026-09-22 11:00:13 +0000

    bedc9177a1813de4975130f32fa252c19d801da4 Release 2.1.0
    Sync to source repo @bedc9177a1813de4975130f32fa252c19d801da4

--- js/dataTables.searchBuilder.mjs
+++ js/dataTables.searchBuilder.mjs
@@ -1,4 +1,4 @@
-/*! SearchBuilder 2.0.1 for DataTables
+/*! SearchBuilder 2.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
@@ -3604,7 +3604,7 @@ class SearchBuilder {
         });
     }
 }
-SearchBuilder.version = '2.0.1';
+SearchBuilder.version = '2.1.0';
 SearchBuilder.classes = {
     button: 'dtsb-button',
     clearAll: 'dtsb-clearAll',
